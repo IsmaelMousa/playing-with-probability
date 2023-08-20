41829343c9e16cbdb837150ae94c25321fc5adb9
Ismael Mousa <ismaelramzimousa@gmail.com>
2023-08-20 17:45:32 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -1,46 +1,22 @@
-# TODO: EDIT MAIN FILE
-from issues import is1, is2
+from utils.config import students_data
+from issues import is1, is2, is3
 
-"""
-def question_1(part: str):
-    prob_avg_of_success = is1.get_prob_avg_success()
-    if part == 'a' or part == 'A':
-        print(prob_avg_of_success)
-    elif part == 'b' or part == 'B':
-        try:
-            trails = int(input("Number of Trails: "))
-            binomial_prob_of_success = is1.get_binomial_prob_of_success(trails=trails,
-                                                                        prob_avg_of_success_in_exam=prob_avg_of_success)
-            print(binomial_prob_of_success)
-
-        except ValueError:
-            print("Invalid input Trails!")
-    return None
-
-
-def question_2(part: str):
-    prob_of_students_gender_is_female = is2.get_prob_of_students_gender_is_female()
-    if part == 'a' or part == 'A':
-        print(prob_of_students_gender_is_female)
-    elif part == 'b' or part == 'B':
-        try:
-            random_variable = int(input("Enter R.V: "))
-            bernoulli_pmf = is2.get_bernoulli_pmf(random_variable=random_variable,
-                                                  prob_of_success=prob_of_students_gender_is_female)
-            print(bernoulli_pmf)
-        except (ValueError, TypeError):
-            print("Invalid input R.V")
-    elif part == 'c' or part == 'C':
-        is2.get_plotting_distribution_of_the_random_variable(
-            prob_of_students_gender_is_female=prob_of_students_gender_is_female)
-    return None
-
-"""
 if __name__ == '__main__':
-    """
-    for part in ['a', 'b']:
-        question_1(part=part)
+    first_question_a = is1.get_prob_avg_success(students_data=students_data)
+    first_question_b = is1.get_binomial_prob_of_success(trails=3, prob_avg_of_success_in_exam=first_question_a)
 
-    for part in ['a', 'b', 'c']:
-        question_2(part=part)
-    """
+    print(first_question_a)
+    print(first_question_b)
+
+    second_question_a = is2.get_prob_of_students_gender_is_female(students_data=students_data)
+    second_question_b = is2.get_bernoulli_pmf(random_variable=int(input("Enter R.V: ")),
+                                              prob_of_success=second_question_a)
+    is2.get_plotting_distribution_of_the_random_variable(prob_of_students_gender_is_female=second_question_a)
+
+    print(second_question_a)
+    print(second_question_b)
+
+    third_question_set = is3.get_student_information()
+    third_question_get = is3.predict_mark(third_question_set, students_data=students_data)
+
+    print(third_question_get)
